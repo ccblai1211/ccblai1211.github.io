c2939ea9d1605e082907a2a5c2ae835c7fffd790
Brian Lai <ccblai1211@gmail.com>
2022-12-12 13:16:26 -0800

    set up auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cvgenerator46f30dd1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 
-const testList = ["ps2", "gcq", "npm"];
-
-function DropDownItem(){
-    return(<li className='dropdownItem'>"test"</li>)
-}
-function listItems(){
-    var listitems = testList.map((d)=> <li className='dropdownItem' key = {d}>{d}</li>);
-    return (listitems);
-}
-
-class DropDown extends React.Component{
-    render(){
-        return(
-
-        <div className ='dropdown'>
-            <button>menu</button>
-            <ul>
-                {listItems()}
-            </ul>
-            <button>menu</button>
-        </div>);
-    }
-}
-
-
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<DropDown />);
+root.render(<App />);
+
+// const testList = ["ps2", "gcq", "npm"];
+
+// function DropDownItem(){
+//     return(<li className='dropdownItem'>"test"</li>)
+// }
+// function listItems(){
+//     var listitems = testList.map((d)=> <li className='dropdownItem' key = {d}>{d}</li>);
+//     return (listitems);
+// }
+
+// class DropDown extends React.Component{
+//     render(){
+//         return(
+
+//         <div className ='dropdown'>
+//             <button>menu</button>
+//             <ul>
+//                 {listItems()}
+//             </ul>
+//             <button>menu</button>
+//         </div>);
+//     }
+// }
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<DropDown />);
